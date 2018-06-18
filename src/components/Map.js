b2b766b7f87b21ed924d49a5b8abd071fe367e22
Garry Clerge <clergeg@gmail.com>
2018-06-18 17:46:18 -0400

    created initial components

---
+++ src/components/Map.js
@@ -0,0 +1,26 @@
+// import React from "react"
+// import { withGoogleMap, GoogleMap } from 'react-google-maps';
+//
+//
+// const map = (props) => {
+//
+//   return (
+//     <div>
+//       <Location
+//         containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
+//         mapElement={ <div style={{ height: `100%` }} /> }
+//       />
+//     </div>
+//   )
+//
+// }
+//
+// export default map
+//
+//
+// const Location = withGoogleMap(props => (
+//      <GoogleMap
+//        defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
+//        defaultZoom = { 13 }
+//      />
+//   ));
---
+++ src/components/RotateLocationContainer.js
@@ -0,0 +1,31 @@
+import React from "react"
+
+// rotating team , staff card,  rot loc container, loc card , message form
+
+
+
+class RotatingLocationContainer extends React.Component {
+  state = {
+    name: "",
+    phoneNumber: "",
+    email: "",
+    message: ""
+  }
+
+
+
+
+
+  render = () => {
+    return (
+      <div>
+        ALL LOCATIONS
+      </div>
+
+    )
+  }
+
+
+}
+
+export default RotatingLocationContainer
