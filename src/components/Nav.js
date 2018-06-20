3393e74687dcd3c97d2ba2bdd1f1f4559132c991
Marvin Clerge <clergemarvin@gmail.com>
2018-06-20 08:38:17 -0400

    nav links and nav sub content

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,12 +13,50 @@ const Nav = props => {
         
         <Grid.Column textAlign='right'>
           <List horizontal>
-            <List.Item>Home</List.Item>
-            <List.Item>About Us</List.Item>
-            <List.Item>Locations</List.Item>
-            <List.Item>Services</List.Item>
-            <List.Item>Learn More</List.Item>
-            <List.Item>Your Visit</List.Item>
+
+            {/* links to id's are refering to pages that would be used */}
+            <List.Item className='nav-link'>
+              <a href="#home">Home</a>
+            </List.Item>
+
+            <List.Item className='nav-link'>
+              <a href="#about">About Us</a>
+            </List.Item>
+
+            <List.Item className='nav-link'>
+              <a href="#locations">Locations</a>
+            </List.Item>
+
+            <List.Item className='nav-link'>
+              <a href="#services">Services</a>
+              <div>
+                <p><a href='#physical'>Physical Therapy</a></p>
+                <p><a href='#chiro'>Chiropractic Care</a></p>
+                <p><a href='#message'>Message Therapy</a></p>
+              </div>
+            </List.Item>
+
+            <List.Item className='nav-link'>
+              <a href="#learn">Learn More</a>
+              <div>
+                <p><a href='#work'>Work Injury</a></p>
+                <p><a href='#accident'>Accident Injury</a></p>
+                <p><a href='#personal'>Personal Injury</a></p>
+                <p><a href='#benefits'>Benefits of Therapy</a></p>
+                <p><a href='#degree'>Chiropractic Degree</a></p>
+                <p><a href='#examiner'>DOT Medical Examiner</a></p>
+              </div>
+            </List.Item>
+
+            <List.Item className='nav-link'>
+              <a href="#visit">Your Visit</a>
+              <div>
+                <p><a href='#forms'>Patient Forms</a></p>
+                <p><a href='#insurance'>Insurance</a></p>
+                <p><a href='#referral'>Referral</a></p>
+              </div>
+            </List.Item>
+            
           </List>
         </Grid.Column>
       </Grid>
